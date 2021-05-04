9baa1210b4ab148b5f4e0efca6e94962a5ea777c
J-Bannach <JohannaBannach@gmail.com>
2021-05-04 17:19:10 +0200

    ✨ Add map function to create characters

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 import "./style.css";
-import { createElement } from "./utils/elements.js";
+import { createElement } from "./utils/elements";
+import { createCharacter } from "./components/character";
+
+const characters = [
+  {
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    origin: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+  },
+  {
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    origin: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+  },
+];
 
 const mainElement = createElement("main", {
   className: "mainPage",
@@ -13,12 +37,12 @@ const mainElement = createElement("main", {
     }),
     createElement("section", {
       className: "resultsSection",
-      children: [createElement("div", { innerText: "No results" })],
+      children: characters.map(createCharacter),
     }),
     createElement("footer", {
       className: "footerSection",
       children: [
-        createElement("p", { innerText: "Look at me, I'm a footer! 👀" }),
+        createElement("p", { innerText: "look at me, I'm a footer!" }),
       ],
     }),
   ],
