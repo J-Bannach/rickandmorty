0a2d3a14db6de0925490011058b8ed7de5f31dfd
J-Bannach <JohannaBannach@gmail.com>
2021-05-05 16:48:24 +0200

    :sparkles: Add debounce function

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import "./style.css";
-import { createElement } from "./utils/elements";
+import { createElement, removeAllChildren } from "./utils/elements";
 import { createCharacter } from "./components/character";
 import { getCharacters } from "./utils/api";
+import { debounce } from "./utils/timer";
 
 const characterSection = createElement("section", {
   className: "resultsSection",
@@ -22,7 +23,20 @@ const mainElement = createElement("main", {
       className: "hero",
       children: [
         createElement("h1", { innerText: "Rick and Morty Fun" }),
-        createElement("input", { placeholder: "search name" }),
+        createElement("input", {
+          className: "input",
+          placeholder: "search name",
+          autofocus: true,
+          oninput: debounce((event) => {
+            removeAllChildren(characterSection);
+
+            const search = event.target.value;
+            getCharacters(search).then((characters) => {
+              const characterElements = characters.map(createCharacter);
+              characterSection.append(...characterElements);
+            });
+          }, 300),
+        }),
       ],
     }),
     characterSection,
